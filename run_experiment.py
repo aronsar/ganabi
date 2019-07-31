769b723acd1e1d5359963e8ced04a3778aa0ed32
Skyler Bala <skyler.bala@gmail.com>
2019-07-31 16:52:09 -0700

    fully working run_experiment

--- run_experiment.py
+++ run_experiment.py
@@ -1,28 +1,30 @@
-# This script is meant as an end to end, data creator, trainer, and evaluater.
-# It is set up so that the tasks within can easily be done manually as well,
-# by splitting up the tasts into separate scripts/modules.
-
-#import statements
-from utils import parse_args, dir_utils
-import create_load_data
-import train
-#import evaluate
-import tensorflow as tf
+"""
+This script is meant as an end to end, data creator, trainer, and evaluater.
+It is set up so that the tasks within can easily be done manually as well,
+by splitting up the tasts into separate scripts/modules.
+"""
+import gin
 import keras
+
+from utils.parse_args import parse
+from train import train_model
+from create_load_data import create_load_data
+from evaluate import evaluate_model
 
 
 def main():
-    args = parse_args.parse()
-    args = dir_utils.resolve_run_directory(args)
+    args = parse()
 
-    #create/load data
-    loader = create_load_data.main(args)
+    gin.external_configurable(keras.optimizers.Adam, module='keras.optimizers')
+    gin.external_configurable(keras.losses.categorical_crossentropy, module='keras.losses')
+    gin.parse_config_file(args.config_path)
 
-    #train model/load model
-    model = train.main(loader, args)
+    data = create_load_data(args)
 
-    #evaluate model
-    evaluate.main(data, model, args)
+    model = train_model(data, args)
+
+    evaluate_model(data, model, args)
+
 
 if __name__ == "__main__":
     main()
