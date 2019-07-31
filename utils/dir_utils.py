eb1e1005da8cda6979d8d5eaa23b43e7dbe202b3
Skyler Bala <skyler.bala@gmail.com>
2019-07-31 16:51:03 -0700

    changes to parse_args to have run_experiment run

--- utils/dir_utils.py
+++ utils/dir_utils.py
@@ -1,27 +1,28 @@
 import os
 
-def find_newrunID(outdir):
-    run_names = [fname for fname in os.listdir(outdir) if "run" in fname]
-    #possible awkward bug: if you put a file inside outdir that is not
-    #of the form "run" followed by a 3 digit number
-    run_names.sort(key=lambda f: int(''.join(filter(str.isdigit, f))))
-    lastrunID = int(''.join(filter(str.isdigit, run_names[-1])))
 
-    return lastrunID + 1 #newrunID
+def get_new_run_id(outdir):
+    last_run_id = 0
+    run_names = [fname for fname in os.listdir(outdir) if "run" in fname]
+    if len(run_names) is not 0:
+        run_names.sort(key=lambda f: int(''.join(filter(str.isdigit, f))))
+        last_run_id = int(''.join(filter(str.isdigit, run_names[-1])))
+
+    return last_run_id + 1
+
 
 def resolve_run_directory(args):
-    # create new run directory if -newrun flag specified
     if args.newrun:
-        newrunID = find_newrunID(args.outdir)
-        os.mkdir(os.path.join(args.outdir, 'run%03d'%newrunID))
-        args.ckptdir = os.path.join(args.outdir, 'run%03d'%newrunID, 'checkpoints/')
-        args.resultdir = os.path.join(args.outdir, 'run%03d'%newrunID, 'results/')
+        new_run_id = get_new_run_id(args.outdir)
+        os.mkdir(os.path.join(args.outdir, 'run%03d' % new_run_id))
+        args.ckptdir = os.path.join(args.outdir, 'run%03d' % new_run_id, 'checkpoints/')
+        args.resultdir = os.path.join(args.outdir, 'run%03d' % new_run_id, 'results/')
         os.mkdir(args.ckptdir)
         os.mkdir(args.resultdir)
-        #TODO: copy over gin config file
-        #TODO: save git commit hash in result directory as well
-
-    if not args.newrun and (args.ckptdir is None or args.resultdir is None):
+        # TODO: copy over gin config file
+        # TODO: save git commit hash in result directory as well
+    elif args.ckptdir is None or args.resultdir is None:
         raise ValueError("Please either specify the -newrun flag "
-                "or provide paths for both --ckptdir and --resultdir")
+                         "or provide paths for both --ckptdir and --resultdir")
     return args
+
