cc2cc13e543ea7520fe051cefd8d680ceae97508
Skyler Bala <skyler.bala@gmail.com>
2019-08-05 17:15:43 -0700

    successful merge

--- train.py
+++ train.py
@@ -1,8 +1,7 @@
-from utils import parse_args
 import importlib
-import load_data
 import gin
-
+import os
+from keras.callbacks import CSVLogger
 
 @gin.configurable
 class TrainConfig(object):
@@ -19,28 +18,6 @@ class TrainConfig(object):
         self.epochs = epochs
 
 
-# def main(data, args):
-# trainer = Trainer(args) # gin configured
-
-# #FIXME: combine into one line once stuff works
-# mode_module = importlib.import_module(args.mode)
-# model = mode_module.build_model(args)
-
-# model.compile(
-# optimizer = trainer.optimizer,
-# loss = trainer.loss,
-# metrics = trainer.metrics)
-
-# tr_history = model.fit_generator(
-# generator = data.generator('train'),
-# verbose = 2, # one line per epoch
-# batch_size = trainer.batch_size,
-# epochs = trainer.epochs, # = total data / batch_size
-# validation_split = 0.1, # fraction of data used for val
-# shuffle = True)
-
-# return model
-
 def train_model(data, args):
     train_config = TrainConfig(args)
     mode_module = importlib.import_module("modes." + args.mode)
@@ -53,13 +30,21 @@ def train_model(data, args):
     model.compile(
         optimizer=train_config.optimizer,
         loss=train_config.loss,
-        metrics=train_config.metrics)
+        metrics=train_config.metrics
+    )
+
+    results_csv_file = os.path.join(args.results_dir, "results.csv")
+
+    results_logger = CSVLogger(results_csv_file, append=True, separator=';')
 
     model.fit_generator(
         generator=train_generator,
         validation_data=val_generator,
         verbose=2,
         epochs=train_config.epochs,
-        shuffle=True)
+        shuffle=True,
+        callbacks=[results_logger]
+    )
 
     return model
+
